275ff345b8a49ba890a97c93037d5008124f199b
aloktiwari007 <alokkumartiwari008@gmail.com>
2026-07-01 19:22:08 +0530

    Initialize Express app with EJS and static middleware
    Set up Express application with EJS view engine and static files.

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
-console.log("Hello from Node.js!");
+const express = require("express");
 
-function add(a, b) {
-    return a + b;
-}
+const app = express();
 
-module.exports = { add };
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+
+    res.render("index", {
+
+        title: "Jenkins Deployment",
+
+        message: "Application deployed successfully using Jenkins + PM2"
+
+    });
+
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+
+    console.log(`Application running on port ${PORT}`);
+
+});
